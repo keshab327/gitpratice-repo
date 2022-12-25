6df30aa3eb1d9ba066b62fe6da31e69913ac6c87
Keshab <keshabjoshi998@gmail.com>
2022-12-25 08:27:17 +0545

    keshab

---
+++ src/test/java/com/example/testhari/TesthariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testhari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesthariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
